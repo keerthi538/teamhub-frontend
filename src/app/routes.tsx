aefd7798c320e5d4e419b56732d728cbd36793c8
keerthi538 <keerthikv.dev@gmail.com>
2026-02-15 19:34:01 +0530

    chore: code cleanup

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -1,11 +1,9 @@
 import Login from "./pages/Login";
 import Dashboard from "./pages/Documents";
-// import EditorPage from "./components/EditorPage";
 import ProtectedRoute from "./ProtectedRoute";
 import AppLayout from "./common/AppLayout";
 import TeamMembers from "./pages/TeamMembers";
 import EditorPage from "./pages/EditorPage";
-// import DocumentEditor from "./pages/EditorPage";
 
 export const routes = [
   {
